8638a75eb14b9b45ac38db3ff182ea7cbf762455
John Weachock <jweachock@gmail.com>
2019-03-03 13:18:15 -0500

    status: Add pretty printing

--- src/status.rs
+++ src/status.rs
@@ -1,3 +1,9 @@
+use ansi_term::Colour::Blue;
+use ansi_term::Colour::Cyan;
+use ansi_term::Colour::Green;
+use ansi_term::Colour::Red;
+use ansi_term::Colour::Yellow;
+use ansi_term::Style;
 use exitfailure::ExitFailure;
 use failure::ResultExt;
 use git2::Repository;
@@ -11,55 +17,64 @@ struct Cli {
   path: std::path::PathBuf,
 }
 
+fn get_status_string(status: &Status) -> String {
+  let index_string = if status.is_index_new() {
+    Green.paint("new")
+  } else if status.is_index_modified() {
+    Green.paint("mod")
+  } else if status.is_index_deleted() {
+    Red.paint("del")
+  } else if status.is_index_renamed() {
+    Blue.bold().paint("ren")
+  } else if status.is_index_typechange() {
+    Blue.paint("typ")
+  } else {
+    Style::new().paint("   ")
+  };
+
+  let working_string = if status.is_wt_new() {
+    Green.bold().paint("new")
+  } else if status.is_wt_modified() {
+    Green.bold().paint("mod")
+  } else if status.is_wt_deleted() {
+    Red.bold().paint("del")
+  } else if status.is_wt_renamed() {
+    Blue.bold().paint("ren")
+  } else if status.is_wt_typechange() {
+    Blue.bold().paint("typ")
+  } else {
+    Style::new().paint("   ")
+  };
+
+  if status.is_ignored() {
+    format!("{}", Cyan.paint(" ignored"))
+  } else if status.is_conflicted() {
+    format!("{}", Red.paint("conflict"))
+  } else {
+    format!(" {} {}", index_string, working_string)
+  }
+}
+
 fn main() -> Result<(), ExitFailure> {
   let args = Cli::from_args();
   env_logger::init();
 
-  let status_opts = StatusOptions::new();
+  let mut status_opts = StatusOptions::new();
+  status_opts.include_untracked(true);
 
   let repo = Repository::open(args.path).with_context(|_| "couldn't open repository")?;
   let _state = repo.state();
 
-  println!("path  index  working");
-  for entry in repo.statuses(None).with_context(|_| "couldn't open status")?.iter() {
+  for entry in repo
+    .statuses(Some(&mut status_opts))
+    .with_context(|_| "couldn't open status")?
+    .iter()
+  {
     let path = entry.path().unwrap();
     let status = entry.status();
-    let index_string = if status.is_index_new() {
-      "new"
-    } else if status.is_index_modified() {
-      "modified"
-    } else if status.is_index_deleted() {
-      "deleted"
-    } else if status.is_index_renamed() {
-      "renamed"
-    } else if status.is_index_typechange() {
-      "typechange"
-    } else {
-      "    "
-    };
-    let working_string = if status.is_wt_new() {
-      "new"
-    } else if status.is_wt_modified() {
-      "modified"
-    } else if status.is_wt_deleted() {
-      "deleted"
-    } else if status.is_wt_renamed() {
-      "renamed"
-    } else if status.is_wt_typechange() {
-      "typechange"
-    } else {
-      "    "
-    };
+    let status_string = get_status_string(&status);
 
-    let status_string = if status.is_ignored() {
-      format!("{} ignored", path)
-    } else if status.is_conflicted() {
-      format!("{} conflicted", path)
-    } else {
-      format!("{} {} {}", path, index_string, working_string)
-    };
-
-    println!("{}", status_string);
+    println!("{} {}", status_string, path);
   }
 
   Ok(())
