cd112844da48001f3d1c87a5fc5a9f9e867b6a69
John Weachock <jweachock@gmail.com>
2019-03-02 20:36:46 -0500

    main: Move from StructOpt to raw clap

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,47 @@
+use clap::crate_version;
+use clap::App;
+use clap::AppSettings;
 use exitfailure::ExitFailure;
 use failure::ResultExt;
 use log::info;
 use std::process::exit;
 use std::process::Command;
 use std::process::Stdio;
-use structopt::StructOpt;
-
-#[derive(StructOpt)]
-struct Cli {
-  command: String,
-}
 
 fn main() -> Result<(), ExitFailure> {
-  let args = Cli::from_args();
   env_logger::init();
 
-  info!("Running subcommand: `milk-{}`", args.command);
+  let args = App::new("milk")
+    .version(crate_version!())
+    .setting(AppSettings::AllowExternalSubcommands)
+    .setting(AppSettings::ColoredHelp)
+    .get_matches();
 
-  let exit_status = Command::new(format!("milk-{}", args.command))
-    .stdin(Stdio::inherit())
-    .stdout(Stdio::inherit())
-    .stderr(Stdio::inherit())
-    //.args(&subcommand_args[..])
-    .spawn()
-    .and_then(|mut handle| handle.wait())
-    .with_context(|_| format!("couldn't execute command: `milk-{}`", args.command))?;
+  match args.subcommand() {
+    (subcommand, Some(scmd)) => {
+      let subcommand_args: Vec<&str> = match scmd.values_of("") {
+        Some(values) => values.collect(),
+        None => Vec::new(),
+      };
 
-  if !exit_status.success() {
-    println!("{} exited with non-zero exit code", args.command);
-    exit(exit_status.code().unwrap_or(1));
+      info!("Running subcommand: `milk-{}`", subcommand);
+
+      let exit_status = Command::new(format!("milk-{}", subcommand))
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .args(&subcommand_args[..])
+        .spawn()
+        .and_then(|mut handle| handle.wait())
+        .with_context(|_| format!("couldn't execute command: `milk-{}`", subcommand))?;
+
+      if !exit_status.success() {
+        eprintln!("{} exited with non-zero exit code", subcommand);
+        exit(exit_status.code().unwrap_or(1));
+      }
+    }
+
+    _ => {}
   }
 
   Ok(())
