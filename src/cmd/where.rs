7cd5b15b715d28c7926449452f0376dbbb09673f
John Weachock <jweachock@gmail.com>
2019-03-05 08:30:42 -0500

    cmd.where: Add basic functionality

---
+++ src/cmd/where.rs
@@ -0,0 +1,28 @@
+use colored::*;
+use exitfailure::ExitFailure;
+use failure::ResultExt;
+use git2::Repository;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+  #[structopt(long = "repo", short = "p", default_value = ".")]
+  repo_path: std::path::PathBuf,
+}
+
+fn main() -> Result<(), ExitFailure> {
+  let args = Cli::from_args();
+  env_logger::init();
+
+  let repo = Repository::discover(args.repo_path).with_context(|_| "couldn't open repository")?;
+
+  match repo.workdir() {
+    Some(path) => match path.to_str() {
+      Some(path_str) => println!("{}", path_str),
+      None => println!("Path is not UTF-8"),
+    },
+    None => println!("Repository is bare."),
+  }
+
+  Ok(())
+}
