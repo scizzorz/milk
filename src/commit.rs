e7fb3dda18fa8d650009333922449dfb6ed78b16
John Weachock <jweachock@gmail.com>
2019-05-06 12:31:24 -0400

    commit: Add $EDITOR support for messages

--- src/commit.rs
+++ src/commit.rs
@@ -1,7 +1,20 @@
+// use std::fs::Open;
+// use std::io::ReadWrite;
+// use std::io::SeekSet;
+// use std::io::TempDir;
+// use std::io::process::InheritFd;
 use colored::*;
 use exitfailure::ExitFailure;
+use failure::Error;
 use failure::ResultExt;
 use git2::Repository;
+use std::env;
+use std::fs::OpenOptions;
+use std::io::SeekFrom;
+use std::io::prelude::*;
+use std::path::Path;
+use std::path::PathBuf;
+use std::process::Command;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -10,6 +23,44 @@ struct Cli {
   /// Path to the repository root
   #[structopt(long = "repo", short = "p", default_value = ".")]
   repo_path: std::path::PathBuf,
+}
+
+fn temporary_editor(path: &Path, contents: &str) -> Result<String, Error> {
+    let mut file = OpenOptions::new()
+      .write(true)
+      .truncate(true)
+      .create(true)
+      .open(path)
+      .with_context(|_| "couldn't open temp commit message file")?;
+
+    file.write_all(contents.as_bytes()).with_context(|_| "couldn't write temp commit message")?;
+    file.sync_all().with_context(|_| "couldn't sync file contents")?;
+
+    // FIXME one of the Err cases here is a non-unicode value... I'd assume you
+    // can run a non-unicode command, no?
+    let editor = match env::var("VISUAL") {
+        Ok(val) => val,
+        Err(_) => {
+            match env::var("EDITOR") {
+                Ok(val) => val,
+                Err(_) => panic!("Neither $VISUAL nor $EDITOR is happy.")
+            }
+        }
+    };
+
+    let mut editor_command = Command::new(editor);
+    editor_command.arg(&path);
+
+    let mut editor_proc = editor_command.spawn().with_context(|_| "couldn't spawn editor")?;
+
+    editor_proc.wait().with_context(|_| "editor failed for some reason")?;
+
+    file.seek(SeekFrom::Start(0)).with_context(|_| "couldn't seek in file")?;
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).with_context(|_| "couldn't read from file")?;
+
+    Ok(contents)
 }
 
 fn main() -> Result<(), ExitFailure> {
@@ -31,10 +82,14 @@ fn main() -> Result<(), ExitFailure> {
 
   let parents = [&commit];
 
-  let message = "HEHH";
+  let mut message_file_path = PathBuf::new();
+  message_file_path.push(repo.path());
+  message_file_path.push("COMMIT_EDITMSG");
+
+  let message = temporary_editor(&message_file_path, "").with_context(|_| "couldn't get message")?;
 
   // FIXME fix commit message
-  repo.commit(Some("HEAD"), &sig, &sig, message, &tree, &parents).with_context(|_| "couldn't write commit")?;
+  repo.commit(Some("HEAD"), &sig, &sig, &message, &tree, &parents).with_context(|_| "couldn't write commit")?;
 
   Ok(())
 }
