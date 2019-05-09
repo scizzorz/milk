346e37f380e41f0db04401112d4690f52fbe2556
John Weachock <jweachock@gmail.com>
2019-05-09 11:12:41 -0400

    cmd: Add dispatching for all Command variants

--- src/cmd.rs
+++ src/cmd.rs
@@ -4,12 +4,21 @@ use failure::Error;
 
 pub fn main(args: cli::Root) -> Result<(), Error> {
   match args.command {
+    Command::Clean(cmd_args) => clean(&args.globals, &cmd_args),
+    Command::Commit(cmd_args) => commit(&args.globals, &cmd_args),
+    Command::Diff(cmd_args) => diff(&args.globals, &cmd_args),
+    Command::Head(cmd_args) => head(&args.globals, &cmd_args),
+    Command::Ignore(cmd_args) => ignore(&args.globals, &cmd_args),
     Command::Init(cmd_args) => init(&args.globals, &cmd_args),
     Command::Ls(cmd_args) => ls(&args.globals, &cmd_args),
-    _ => {
-      println!("Can't run mystery command.");
-      Ok(())
-    }
+    Command::Me(cmd_args) => me(&args.globals, &cmd_args),
+    Command::Restore(cmd_args) => restore(&args.globals, &cmd_args),
+    Command::Show(cmd_args) => show(&args.globals, &cmd_args),
+    Command::Stage(cmd_args) => stage(&args.globals, &cmd_args),
+    Command::Status(cmd_args) => status(&args.globals, &cmd_args),
+    Command::Tag(cmd_args) => tag(&args.globals, &cmd_args),
+    Command::Unstage(cmd_args) => unstage(&args.globals, &cmd_args),
+    Command::Where(cmd_args) => where_(&args.globals, &cmd_args),
   }?;
 
   Ok(())
