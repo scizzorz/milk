07c557da60de7689a0d78bd57a871352e738c56a
John Weachock <jweachock@gmail.com>
2019-05-12 23:15:30 -0400

    branch-rename: Migrate into cmd

--- src/cmd.rs
+++ src/cmd.rs
@@ -393,6 +393,35 @@ pub fn branch_new(globals: cli::Global, args: cli::BranchNew) -> Result<(), Erro
 }
 
 pub fn branch_rename(globals: cli::Global, args: cli::BranchRename) -> Result<(), Error> {
+  let repo = Repository::discover(globals.repo_path).with_context(|_| "couldn't open repository")?;
+
+  let typ = if args.is_remote {
+    BranchType::Remote
+  } else {
+    BranchType::Local
+  };
+
+  let mut branch = repo
+    .find_branch(&args.src_name, typ)
+    .with_context(|_| "couldn't find branch")?;
+
+  branch
+    .rename(&args.dest_name, args.force)
+    .with_context(|_| "couldn't rename branch")?;
+
+  let target = branch
+    .get()
+    .target()
+    .ok_or_else(|| failure::err_msg("couldn't get branch target"))?;
+
+  let commit = repo
+    .find_commit(target)
+    .with_context(|_| "couldn't find commit")?;
+
+  println!("Renamed branch {} => {}", args.src_name, args.dest_name);
+  println!("{}", highlight_named_oid(&repo, &args.dest_name, target));
+  print_commit(&repo, &commit);
+
   Ok(())
 }
 
